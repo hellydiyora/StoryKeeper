2283c2d41a740992cf1fcc9a588e1dc4131d2507
hellydiyora <diyorahelly@gmail.com>
2025-05-03 15:18:53 +0530

    Cahneg for vercel

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Recommendations from './pages/Recommendations';
 import Affirmations from './pages/Affirmations';
@@ -8,13 +8,15 @@ import NotFound from './pages/NotFound';
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/recommendations" element={<Recommendations />} />
-      <Route path="/affirmations" element={<Affirmations />} />
-      <Route path="/blogs" element={<Blogs />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recommendations" element={<Recommendations />} />
+        <Route path="/affirmations" element={<Affirmations />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
